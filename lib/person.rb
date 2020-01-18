57c21dc1dc81b93e1658b74546d8414123e36c72
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-18 23:17:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,10 @@ class Person
   def talk 
     puts "Hello World!"
   end 
+<<<<<<< HEAD
   def walk
     puts "The Person is walking"
   end 
+=======
+>>>>>>> 4cec352e780c865425ab210367ff0534b2acc951
 end 
